51600058e126f03d38fe109b2f78a3233c179279
timsprunger <tim@reusserdesign.com>
2020-02-17 18:44:58 -0500

    Re-Design Raspberry Pi Weather Project

--- Raspberry Pi Bootup Weather Project (HTML,CSS,PHP)/Application Files/index.php
+++ Raspberry Pi Bootup Weather Project (HTML,CSS,PHP)/Application Files/index.php
@@ -3,48 +3,41 @@
 <html>
 
 <head>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="pirates.css">
-<title>Huntington Weather</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="pirates.css">
+	<title>Huntington Weather</title>
+	<link href="https://fonts.googleapis.com/css?family=Bad+Script&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
-<h1>Huntington, Indiana</h1> 
+<div class="flex">
+	<img src="map_of_huntington_in.jpg" alt="HU_photo" align="right" width="428" height="428">
 
-<p>_____________________________________________</p>
+	<div class="data">
+		<h1>Huntington, Indiana</h1> 
 
-<img src="map_of_huntington_in.jpg" alt="HU_photo" align="right" width="428" height="428">
+		<?php
+			$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=4921729&APPID=bd5398218654d346ee7a1d2c2d119ba0&units=imperial');
+			
+			$data = json_decode($json);
 
-<?php
+			echo '<h3>Geographic Location</h3>';
+			echo '<p>Longitude: ', $data->coord->lon, '&deg; </p>';
+			echo '<p>Latitude: ', $data->coord->lat, '&deg; </p>';
+			echo '<h3>Current Description</h3>';
 
-$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=4921729&APPID=bd5398218654d346ee7a1d2c2d119ba0&units=imperial');
-$data = json_decode($json);
+			foreach ($data->weather as $weather) {
+				echo '<p>', $weather->description, '</p>';
+			}
 
-echo '<h2>Geographic Location</h2>';
-
-echo '<p>Longitude: ', $data->coord->lon, '&deg; </p>';
-
-echo '<p>Latitude: ', $data->coord->lat, '&deg; </p>';
-
-echo '<h2>Current Description</h2>';
-
-foreach ($data->weather as $weather) {
-echo '<p>', $weather->description, '</p>';
-}
-
-echo '<h2>Current Conditions</h2>';
-
-echo '<p>Current Temp: ', $data->main->temp, '&deg; F</p>';
-
-echo '<p>Humidity: ', $data->main->humidity, '%</p>';
-
-echo '<p>Wind Speed: ', $data->wind->speed, ' m/h</p>';
-
-echo '<p>Clouds: ', $data->clouds->all, '%</p>';
-
-?>
-
+			echo '<h3>Current Conditions</h3>';
+			echo '<p>Current Temp: ', $data->main->temp, '&deg; F</p>';
+			echo '<p>Humidity: ', $data->main->humidity, '%</p>';
+			echo '<p>Wind Speed: ', $data->wind->speed, ' m/h</p>';
+			echo '<p>Clouds: ', $data->clouds->all, '%</p>';
+		?>
+	</div>
 </body>
 </html>
 
